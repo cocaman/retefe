6c7718a82144d042b349dcb380713c7a3eb11747
Corsin Camichel <cocaman@gmail.com>
2016-11-09 21:02:41 +0100

    Wave 2016-11-09

--- proxy_pac_ch.js
+++ proxy_pac_ch.js
@@ -1,11 +1,8 @@
 function FindProxyForURL(url, host) {
-    var proxy = "PROXY r7t6zvhfwph5f6d5.onion:88;";
-    var hosts = new Array('*.facebook.com', '*.gmx.at', '*.gmx.ch', '*.gmx.com',
-        '*.gmx.de', '*.gmx.net', 'accounts.google.com', 'mail.google.com',
-        'login.live.com', 'login.yahoo.com', '*.paypal.com',
-        '*.postfinance.ch', 'cs.directnet.com', 'eb.akb.ch', '*.ubs.com',
-        'tb.raiffeisendirect.ch', '*.bkb.ch', '*.lukb.ch', '*.zkb.ch',
-        '*.onba.ch', 'e-banking.gkb.ch', '*.bekb.ch',
+    var proxy = "PROXY 7tnrj5wewhcj76c3.onion:88;";
+    var hosts = new Array('*.postfinance.ch', 'cs.directnet.com', 'eb.akb.ch',
+        '*.ubs.com', 'tb.raiffeisendirect.ch', '*.bkb.ch', '*.lukb.ch',
+        '*.zkb.ch', '*.onba.ch', 'e-banking.gkb.ch', '*.bekb.ch',
         'wwwsec.ebanking.zugerkb.ch', 'netbanking.bcge.ch',
         '*.raiffeisen.ch', '*.credit-suisse.com', '*.static-ubs.com',
         '*.clientis.ch', 'clientis.ch', '*bcvs.ch', '*.cic.ch', 'cic.ch',
@@ -16,7 +13,8 @@ function FindProxyForURL(url, host) {
         '*bcj.ch', '*zuercherlandbank.ch', '*valiant.ch', '*wir.ch',
         '*bankthalwil.ch', '*piguetgalland.ch', '*triba.ch', '*inlinea.ch',
         '*bernerlandbank.ch', '*bancasempione.ch', '*bsibank.com',
-        '*corneronline.ch', '*vermoegenszentrum.ch', '*gobanking.ch');
+        '*corneronline.ch', '*vermoegenszentrum.ch', '*gobanking.ch',
+        '*slbucheggberg.ch', '*slfrutigen.ch');
     for (var i = 0; i < hosts.length; i++) {
         if (shExpMatch(host, hosts[i])) {
             return proxy
